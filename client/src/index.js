f255f046972daf80e7892b58be595b44f1fab26e
March Miskin <marchmiskin@gmail.com>
2023-12-17 16:24:57 -0800

    Completed Lesson 257

--- client/src/index.js
+++ client/src/index.js
@@ -1,2 +1,6 @@
 import "@fortawesome/fontawesome-free/css/all.css";
+import Modal from "./components/Modal";
 import "./css/style.css";
+// import { model } from "mongoose";
+
+const modal = new Modal();
